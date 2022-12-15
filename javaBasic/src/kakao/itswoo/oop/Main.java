7bb1c2ad7ad2b02c29cb4567ba33061f9771e3f4
kjk04021 <kjk04021@naver.com>
2022-12-15 16:49:12 +0900

    221215JavaClass

---
+++ javaBasic/src/kakao/itswoo/oop/Main.java
@@ -0,0 +1,16 @@
+package kakao.itswoo.oop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Student 클래스의 인스턴스 생성
+		Student student = new Student();
+		student.num=1;
+		
+		
+		
+	}
+	
+	
+
+}
---
+++ javaBasic/src/kakao/itswoo/oop/Student.java
@@ -0,0 +1,17 @@
+package kakao.itswoo.oop;
+
+public class Student {
+	public int num;
+	static String schoolName;
+	// static 초기화
+	static {
+		System.out.println("고양이");
+		schoolName="감정중학교";
+		//num=1;
+	}
+	public int score=100;
+	
+	
+	
+
+}
